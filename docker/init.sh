5d454998268b22e0fcbb2b9538611e7970d894a2
David Amigo <david.amigo@privalia.com>
2018-03-23 09:54:50 +0100

    Docker scripts - Added Basket service

--- docker/init.sh
+++ docker/init.sh
@@ -6,4 +6,5 @@ cd $dir
 docker exec -it event_bus_user_database mysql -uroot -proot -D user_service \
     -e "CREATE TABLE IF NOT EXISTS user_service.user (id INT NOT NULL, global_id INT NOT NULL DEFAULT 0, first_name VARCHAR(48) NOT NULL, last_name VARCHAR(72) NOT NULL, address VARCHAR(128) NULL DEFAULT NULL, PRIMARY KEY (id));"
 
-
+docker exec -it event_bus_basket_database mysql -uroot -proot -D basket_service \
+    -e "CREATE TABLE IF NOT EXISTS basket_service.user (id INT NOT NULL, global_id INT NOT NULL DEFAULT 0, PRIMARY KEY (id));"
