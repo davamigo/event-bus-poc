4854cf1cbd07f65b20a6bb2e2aca8d7f928d4a16
David Amigo <david.amigo@privalia.com>
2018-03-26 10:40:54 +0200

    Basket Service - Setup 2 consumers for the same queue (workers)

--- BasketService/src/main/java/com/privalia/poc/eventbus/basket/eventbus/EventBusConfig.java
+++ BasketService/src/main/java/com/privalia/poc/eventbus/basket/eventbus/EventBusConfig.java
@@ -18,39 +18,55 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class EventBusConfig {
 
+    /** Exchange name read from application.yml */
     @Value("${eventbus.objects.exchange}")
     private String exchangeName;
 
+    /** Queue name read from application.yml */
     @Value("${eventbus.objects.queue}")
     private String queueName;
 
+    /** Declaration of the exchange object */
     @Bean
     public DirectExchange eventBusExchange() {
         return new DirectExchange(exchangeName, true, false);
     }
 
+    /** Declaration of the queue object */
     @Bean
     public Queue eventBusQueue() {
         return new Queue(queueName, true, false, false);
     }
 
-    @Bean
-    public Binding eventBusBindingUserCreated(DirectExchange eventBusExchange, Queue eventBusQueue) {
-        return BindingBuilder.bind(eventBusQueue).to(eventBusExchange).with((UserCreated.bindingName()));
+    /** Bind exchange to the queue with the event names as keys  */
+    private static class EventBusBindingsConfig {
+        @Bean
+        public Binding eventBusBindingUserCreated(DirectExchange eventBusExchange, Queue eventBusQueue) {
+            return BindingBuilder.bind(eventBusQueue).to(eventBusExchange).with((UserCreated.bindingName()));
+        }
+
+        @Bean
+        public Binding eventBusBindingUserUpdated(DirectExchange eventBusExchange, Queue eventBusQueue) {
+            return BindingBuilder.bind(eventBusQueue).to(eventBusExchange).with((UserUpdated.bindingName()));
+        }
     }
 
-    @Bean
-    public Binding eventBusBindingUserUpdated(DirectExchange eventBusExchange, Queue eventBusQueue) {
-        return BindingBuilder.bind(eventBusQueue).to(eventBusExchange).with((UserUpdated.bindingName()));
-    }
-
+    /** The event publisher object */
     @Bean
     public EventBusPublisher eventBusPublisher() {
         return new EventBusPublisher();
     }
 
-    @Bean
-    public EventBusConsumer eventBusConsumer() {
-        return new EventBusConsumer();
+    /** The event consumer obects (workers) */
+    private static class EventBusConsumerConfig {
+        @Bean
+        public EventBusConsumer eventBusConsumerWorker1() {
+            return new EventBusConsumer();
+        }
+
+        @Bean
+        public EventBusConsumer eventBusConsumerWorker2() {
+            return new EventBusConsumer();
+        }
     }
 }
